116f4de6e952b092abcd71207cc40c0e0e44a10d
NaashNix <thilakshanaravindu@gmail.com>
2022-11-21 00:16:13 +0530

    Some files updated

--- src/Components/MenuBar.jsx
+++ src/Components/MenuBar.jsx
@@ -3,10 +3,28 @@ import classes from './MenuBar.module.css';
 
 const MenuBar = () => {
    return (
-      <div>
-         
-      </div>
-   );
+		<div className={classes.menuBarContainer}>
+			<span className={classes.logo}>NAASHNIX</span>
+			<div>
+				<a className={classes.menuItems} href="">
+					HOME
+				</a>
+				<a className={classes.menuItems} href="">
+					ABOUT ME
+				</a>
+				<a className={classes.menuItems} href="">
+					EDUCATION
+				</a>
+				<a className={classes.menuItems} href="">
+					PROJECTS
+				</a>
+				<a className={classes.menuItems} href="">
+					CONTACT
+				</a>
+			</div>
+			<a href="">START A PROJECT</a>
+		</div>
+	);
 }
 
 export default MenuBar;
--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import MenuBar from '../Components/MenuBar';
 import classes from './Home.module.css';
 
 const Home = () => {
    return (
       <div>
-
+         <MenuBar />
       </div>
    );
 }
