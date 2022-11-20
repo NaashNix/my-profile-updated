91d8fec2f4109e17791aa78172d0e5107b6a8333
NaashNix <thilakshanaravindu@gmail.com>
2022-11-20 15:31:42 +0530

    Initial Commit

---
+++ src/Components/MenuBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './MenuBar.module.css';
+
+const MenuBar = () => {
+   return (
+      <div>
+         
+      </div>
+   );
+}
+
+export default MenuBar;
